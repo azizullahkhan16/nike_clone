f579ec14083877c6f4c6fa72239f807b72105739
azizullahkhan16 <aziz.bin.aman16@gmail.com>
2024-01-19 13:01:22 +0500

    Minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
-export default App = () => {
-  return <div className="text-3xl font-bold underline">App</div>;
+const App = () => {
+  return <div className="text-3xl font-bold underline">section</div>;
 };
+
+export default App;
